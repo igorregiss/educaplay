142e878db0a2ec4207de016ec554c30bf15f788d
Igor Régis <igorregis8@gmail.com>
2023-10-16 11:17:35 -0300

    Adicionado somar ao jogar.php e livro.php

--- livro.php
+++ livro.php
@@ -36,7 +36,63 @@ if (isset($_GET['id'])) {
   header('Location: pagina_de_erro.php');
   exit();
 }
+
+// Verifique se o usuário está logado
+if (isset($_SESSION['id'])) {
+  $idDoUsuario = $_SESSION['id'];
+
+  // Conexão com o banco de dados (substitua as informações de conexão)
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "educaplay";
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Verificar a conexão com o banco de dados
+  if ($conn->connect_error) {
+      die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+  }
+
+  // Verificar se o usuário já visitou a página
+  if (isset($_SESSION['ultima_visita'])) {
+      $tempoDecorrido = time() - $_SESSION['ultima_visita'];
+  } else {
+      $tempoDecorrido = 0;
+  }
+
+  // Definir o tempo mínimo entre as atualizações (em segundos)
+  $tempoMinimo = 120; // 10 segundos (ou o valor desejado)
+
+  // Verificar se o tempo decorrido é maior ou igual ao tempo mínimo
+  if ($tempoDecorrido >= $tempoMinimo) {
+      // Atualizar a hora da última visita
+      $_SESSION['ultima_visita'] = time();
+
+      // Incrementar a coluna totalJogos na tabela usuarios
+      $sql = "UPDATE usuarios SET totalLivros = totalLivros + 1 WHERE id = ?";
+      $stmt = $conn->prepare($sql);
+
+      $stmt->bind_param("i", $idDoUsuario);
+
+      if ($stmt->execute()) {
+          echo "Total de livros incrementado para o usuário com ID " . $idDoUsuario;
+      } else {
+          echo "Erro na atualização do total de livros: " . $conn->error;
+      }
+
+      $stmt->close();
+  } else {
+      echo "Tempo mínimo entre as atualizações não foi atingido.";
+  }
+
+  // Fechar a conexão com o banco de dados
+  $conn->close();
+} else {
+  echo "Usuário não está logado."; // Ou redirecione para a página de login
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="pt-br">
