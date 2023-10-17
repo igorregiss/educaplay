6fbfd77a5d1c2cd678e20da1367bea77cb21a3c9
Igor Régis <igorregis8@gmail.com>
2023-10-17 08:22:59 -0300

    Ajustando formatação.

--- jogar.php
+++ jogar.php
@@ -62,7 +62,7 @@ if (isset($_SESSION['id'])) {
   }
 
   // Definir o tempo mínimo entre as atualizações (em segundos)
-  $tempoMinimo = 120; // 10 segundos (ou o valor desejado)
+  $tempoMinimo = 0; // 10 segundos (ou o valor desejado)
 
   // Verificar se o tempo decorrido é maior ou igual ao tempo mínimo
   if ($tempoDecorrido >= $tempoMinimo) {
@@ -76,14 +76,11 @@ if (isset($_SESSION['id'])) {
       $stmt->bind_param("i", $idDoUsuario);
 
       if ($stmt->execute()) {
-          echo "Total de jogos incrementado para o usuário com ID " . $idDoUsuario;
       } else {
-          echo "Erro na atualização do total de jogos: " . $conn->error;
       }
 
       $stmt->close();
   } else {
-      echo "Tempo mínimo entre as atualizações não foi atingido.";
   }
 
   // Fechar a conexão com o banco de dados
@@ -97,25 +94,26 @@ if (isset($_SESSION['id'])) {
 <!DOCTYPE html>
 <html lang="pt-br">
 
-  <head>
+<head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 
     <title>EducaPlay - Aprenda online</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     <link rel="stylesheet" href="assets/css/templatemo-cyborg-gaming.css">
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/animate.css">
-    <link rel="stylesheet"href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
     <link rel="stylesheet" href="assets/css/login.css">
 
     <!--
@@ -130,52 +128,53 @@ TemplateMo 579 Cyborg Gaming
 https://templatemo.com/tm-579-cyborg-gaming
 
 -->
-  </head>
+</head>
 
 <body>
 
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+    <!-- ***** Preloader Start ***** -->
+    <div id="js-preloader" class="js-preloader">
+        <div class="preloader-inner">
+            <span class="dot"></span>
+            <div class="dots">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
     </div>
-  </div>
-<!-- ***** Preloader End ***** -->
+    <!-- ***** Preloader End ***** -->
 
     <!-- ***** Header Area Start ***** -->
     <header class="header-area header-sticky">
-      <div class="container">
-          <div class="row">
-              <div class="col-12">
-                  <nav class="main-nav">
-                      <!-- ***** Logo Start ***** -->
-                      <a href="index.php" class="logo">
-                          <img src="assets/images/logo.png" alt="">
-                      </a>
-                      <!-- ***** Logo End ***** -->
-                      <!-- ***** Search End ***** -->
-                      <div class="search-input">
-                        <form id="search" action="#">
-                          <input type="text" placeholder="Buscar" id='searchText' name="searchKeyword" onkeypress="handle" />
-                          <i class="fa fa-search"></i>
-                        </form>
-                      </div>
-                      <!-- ***** Search End ***** -->
-                      <!-- ***** Menu Start ***** -->
-                      <ul class="nav">
-                      <li><a href="index.php">Inicio</a></li>
-                      <li><a class="active" href="jogos.php">Jogos</a></li>
-                      <li><a  href="livros.php">Livros</a></li>
-                      <li><a href="videos.php">Videos</a></li>
-                      <li><a href="contato.php">Sobre</a></li>
-                      <li><a href="contato.php">Contato</a></li>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.php" class="logo">
+                            <img src="assets/images/logo.png" alt="">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Search End ***** -->
+                        <div class="search-input">
+                            <form id="search" action="#">
+                                <input type="text" placeholder="Buscar" id='searchText' name="searchKeyword"
+                                    onkeypress="handle" />
+                                <i class="fa fa-search"></i>
+                            </form>
+                        </div>
+                        <!-- ***** Search End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="index.php">Inicio</a></li>
+                            <li><a class="active" href="jogos.php">Jogos</a></li>
+                            <li><a href="livros.php">Livros</a></li>
+                            <li><a href="videos.php">Videos</a></li>
+                            <li><a href="contato.php">Sobre</a></li>
+                            <li><a href="contato.php">Contato</a></li>
 
-                      <?php
+                            <?php
       // Verificar se o usuário está logado
       if (isset($_SESSION['nome'])) {
         // Usuário está logado
@@ -187,88 +186,91 @@ https://templatemo.com/tm-579-cyborg-gaming
         echo '<li class="login-link"> <a href="login.php"><i class="bi bi-person-fill"></i> Entrar</a></li>';
     }
     ?>
-?>
-                    </ul>      
-                      <a class='menu-trigger'>
-                          <span>Menu</span>
-                      </a>
-                      <!-- ***** Menu End ***** -->
-                  </nav>
-              </div>
-          </div>
-      </div>
+                            ?>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
     </header>
     <!-- ***** Header Area End ***** -->
 
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="page-content">
-
-
-
-          <!-- ***** Details Start ***** -->
-          <div class="game-details">
-            <div class="row">
-            
-              <div class="container">
-                <div class="row">
-                
-          <!-- ***** Featured Start ***** -->
-
-              <div class="header-text">
-
-
-              <div class="col-sm-6 col-xs-6 col-md-6 mx-auto text-center">
-                    <?php echo $jogos['iframe']; ?>
-                    </div>
-
-
-
-          </div>
-
-
-
-
-          </div>
-          <!-- ***** Featured End ***** -->
-
-
-                   
-          <!-- ***** Details End ***** -->
-
-
-        </div>
-      </div>
-    </div>
-  </div>
-  
-  <footer>
     <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p>Copyright © 2023 <a href="#">EducaPlay</a>. Todos os direitos reservados. 
-          
-          <br>Desenvolvido por: <a href="https://igorregis.com" target="_blank" title="Portfolio de Igor Régis">Igor Régis</a>, Junior Lima, Verocina Lima, Maria Vanessa, Catiana Araujo e Elika Silva.</p>
-        </div>
-      </div>
-    </div>
-  </footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="page-content">
 
 
 
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+                    <!-- ***** Details Start ***** -->
+                    <div class="game-details">
+                        <div class="row">
 
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
+                            <div class="container">
+                                <div class="row">
+
+                                    <!-- ***** Featured Start ***** -->
+
+                                    <div class="header-text">
 
 
-  </body>
+                                        <div class="col-sm-6 col-xs-6 col-md-6 mx-auto text-center">
+                                            <?php echo $jogos['iframe']; ?>
+                                        </div>
+
+
+
+                                    </div>
+
+
+
+
+                                </div>
+                                <!-- ***** Featured End ***** -->
+
+
+
+                                <!-- ***** Details End ***** -->
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <footer>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <p>Copyright © 2023 <a href="#">EducaPlay</a>. Todos os direitos reservados.
+
+                                    <br>Desenvolvido por: <a href="https://igorregis.com" target="_blank"
+                                        title="Portfolio de Igor Régis">Igor Régis</a>, Junior Lima, Verocina Lima,
+                                    Maria Vanessa, Catiana Araujo e Elika Silva.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+
+
+
+                <!-- Scripts -->
+                <!-- Bootstrap core JavaScript -->
+                <script src="vendor/jquery/jquery.min.js"></script>
+                <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+                <script src="assets/js/isotope.min.js"></script>
+                <script src="assets/js/owl-carousel.js"></script>
+                <script src="assets/js/tabs.js"></script>
+                <script src="assets/js/popup.js"></script>
+                <script src="assets/js/custom.js"></script>
+
+
+</body>
 
 </html>
